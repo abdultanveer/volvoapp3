34d1593ea7c9c0785f9bf554ddac708111e27665
abdul <abdul.tanveer@gmail.com>
2020-12-12 12:58:54 +0530

    Initial commit--fragments

---
+++ app/src/main/java/com/next/volvoapp3/HeadlinesFragment.java
@@ -0,0 +1,23 @@
+package com.next.volvoapp3;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HeadlinesFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+         super.onCreateView(inflater, container, savedInstanceState);
+         View fragmentView = inflater.inflate(R.layout.fragment_headlines,container,false);
+        return fragmentView;
+    }
+}
